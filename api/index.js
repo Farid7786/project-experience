73ba2d60f88a842c9e5efdca3be2451c32ec7cc2
shaik farid <sf6074779@gmail.com>
2024-03-31 15:04:52 +0530

    create sign up route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
-import express  from "express";
+import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
-dotenv.config();
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected to mongodb');
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to MongoDb')
 })
-.catch((err) => {
+.catch((err)=>{
     console.log(err);
-});
-const app = express();
-app.listen( 3000, () => {
-    console.log('server is running on port 3000!!');
-});
-app.use("/api/user", userRouter)
+})
+const app = express()
+app.listen(5000,(req,res)=>{
+    console.log('Server is running on port number 5000.....')
+})
+app.use(express.json())
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes)
---
+++ api/routes/auth.route.js
@@ -0,0 +1,6 @@
+import express, { Router } from 'express'
+import { signup } from '../controler/auth.controler.js'
+
+const router = express.Router();
+router.post('/signup',signup)
+export default router
