a9bf3e52ebd3c10f9508cb62456b3fd9940d5421
shaik farid <sf6074779@gmail.com>
2024-04-17 09:08:21 +0530

    create listing page functionality

--- api/routes/listing.Route.js
+++ api/routes/listing.Route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createListing } from '../controler/listing.controler.js';
 import { verifyToken } from '../verification/verifyUser.js';
-const router =express.Router();
+const router = express.Router();
 router.post('/create',verifyToken, createListing);
 export default router;
