63b795a106e7b1cf927a4f5fb89353cc8624cc31
shaik farid <sf6074779@gmail.com>
2024-04-17 14:57:27 +0530

    completed delete listing functionality

--- api/routes/listing.Route.js
+++ api/routes/listing.Route.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { createListing } from '../controler/listing.controler.js';
+import { createListing , deleteListing} from '../controler/listing.controler.js';
 import { verifyToken } from '../verification/verifyUser.js';
 const router = express.Router();
 router.post('/create',verifyToken, createListing);
+router.delete('/delete/:id',verifyToken,deleteListing);
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,9 @@ function Profile() {
   const [filePerc,setFilePerc] =useState(0);
   const [fileUploadError,setFileUploadError] = useState(false);
   const [formData,setFormData]= useState({});
+  const [showListingsError,setShowListingsError]= useState(false);
+  const [userListings,setUserListings] = useState([])
+  const [deleteError,setDeleteError] = useState(false);
   const dispatch = useDispatch();
   console.log(filePerc);
   console.log(fileUploadError);
@@ -98,6 +101,35 @@ function Profile() {
       dispatch(signOutUserFailure(error.message))
     }
   }
+  const handleShowListings= async()=>{
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listing/${currentUser._id}`);
+      const data = await res.json();
+      if(data.sucess === false){
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+  const handleListingDelete = async (ListingId)=>{
+    try {
+      const res = await fetch(`/api/listing/delete/${ListingId}`,{
+        method : 'DELETE',
+      });
+      const data = await res.json();
+      if(data.sucess ===false){
+        setDeleteError(true);
+        return;
+      }
+      setUserListings((prev)=> prev.filter((listing)=> listing._id !== ListingId))
+    } catch (error) {
+      setDeleteError(true);
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center'>Profile</h1>
@@ -133,6 +165,27 @@ function Profile() {
           <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
         </div>
       <p className='text-red-700 mt-5'>{error ? error.message : ' '}</p>
+      <button onClick={handleShowListings} className='text-green-700 items-center'> Show Listings</button>
+      <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings' : ''}</p>
+      {userListings && userListings.length > 0 && 
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-center font-semibold text-2xl mt-7'>Your Listings</h1>
+      {userListings.map((listing)=>(
+      <div key={listing._id} className='flex items-center justify-between gap-4 border p-3 rounded-lg border-slate-500'>
+        <Link to={`/listing/${listing._id}`}>
+          <img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain'/>
+        </Link>
+        <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
+          <p>{listing.name}</p>
+        </Link>
+        <div className='flex flex-col items-center'>
+          <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase hover:opacity-60'>Delete</button>
+          <button className='text-green-700 uppercase hover:opacity-60'>Edit</button>
+        </div>
+      </div>
+      ))}
+      </div>
+      }
     </div>
   ) 
 }
