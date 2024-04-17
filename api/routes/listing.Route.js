0601148c52e7a9f6731f5abca5c2ab77e5227fa6
shaik farid <sf6074779@gmail.com>
2024-04-17 18:37:27 +0530

    create update listing functionality

--- api/routes/listing.Route.js
+++ api/routes/listing.Route.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { createListing , deleteListing} from '../controler/listing.controler.js';
+import { createListing , deleteListing, getListing, updateListing} from '../controler/listing.controler.js';
 import { verifyToken } from '../verification/verifyUser.js';
 const router = express.Router();
 router.post('/create',verifyToken, createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
+router.post('/update/:id',verifyToken,updateListing);
+router.get('/get/:id',getListing);
 export default router;
